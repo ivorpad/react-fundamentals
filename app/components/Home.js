9f5dccc4452a7012566fde42e3074fba44ec03b3
ivorpad <ivorjpc@gmail.com>
2016-06-06 00:06:32 -0430

    Added routing and refactored PromptContainer

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,7 @@
 var React = require('react');
 var transparentBg = require('../styles').transparentBg;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 var Home = React.createClass({
 
@@ -8,6 +10,9 @@ var Home = React.createClass({
       <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
         <h1>Github Battle</h1>
         <p className="lead">Some fancy motto</p>
+        <Link to="/playerOne">
+          <button type="button" className='btn btn-lg btn-success'>Get Started</button>
+        </Link>
       </div>
     );
   }
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,31 +1,55 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
 
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  getInitialState: function() {
+    return {
+      username: '' 
+    };
+  },
+
+  handleUpdateUser: function(e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
+
+  handleSubmitUser: function(e) {
+    e.preventDefault();
+
+    var username = this.state.username;
+    this.setState({
+      username: '' 
+    });
+
+    if(this.props.routeParams.playerone) {
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+
+
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
+  
   render: function() {
-    console.log(transparentBg)
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h1>{this.props.route.header}</h1>
-        <div className="col-sm-12">
-          <form action="">
-            <div className="form-group">
-              <input 
-                type="text"
-                className="form-control"
-                placeholder="Github Username"
-              />
-            </div>
-            <div className="form-group-col-sm-4 col-sm-offset-4">
-              <button className="btn btn-blocl btn-success"
-                      type="submit"  >
-                Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username}
+       />
     );
   }
 
