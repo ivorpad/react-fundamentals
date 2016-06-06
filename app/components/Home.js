8967b02f8ecef3c8fa587a11540eb7ecb2ce5a74
ivorpad <ivorjpc@gmail.com>
2016-06-05 22:50:43 -0430

    Initial commit

---
+++ app/components/Home.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+
+var Home = React.createClass({
+
+  render: function() {
+    return (
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h1>Github Battle</h1>
+        <p className="lead">Some fancy motto</p>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Home;
